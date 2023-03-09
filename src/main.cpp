b13d71ec16738260cc6ea09abca09fc662205797
svorontsov <svorontsov@inobitec.com>
2023-03-09 20:35:59 +0300

    Рефакторинг кода
    - Исправление читабельности и эллементарных ошибок.

--- src/main.cpp
+++ src/main.cpp
@@ -10,39 +10,32 @@
 #include "AudioNotePlayback.h"
 #include "AudioNoteCreator.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-
-    qmlRegisterType<Scanner>("AudioNotes", 1, 0, "Scanner");
-    qmlRegisterType<AudioNotesApp>("AudioNotes", 1, 0, "AudioNotesApp");
-    qmlRegisterType<AudioNotesRepo>("AudioNotes", 1, 0, "AudioNotesRepo");
-    qmlRegisterType<AudioNotesReposModel>("AudioNotes", 1, 0, "AudioNotesReposModel");
-    qmlRegisterType<AudioNote>("AudioNotes", 1, 0, "AudioNote");
-    qmlRegisterType<AudioNotesModel>("AudioNotes", 1, 0, "AudioNotesModel");
-    qmlRegisterType<AudioNotePlayback>("AudioNotes", 1, 0, "AudioNotePlayback");
-    qmlRegisterType<AudioNoteCreator>("AudioNotes", 1, 0, "AudioNoteCreator");
-
-
-    AudioNotesApp* notesApp = new AudioNotesApp();
-
-    engine.rootContext()->setContextProperty("notesApp", notesApp);
-
-    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
-        [url](QObject *obj, const QUrl &objUrl) {
-            if (!obj && url == objUrl)
-                QCoreApplication::exit(-1);
-        },
-        Qt::QueuedConnection);
-    engine.load(url);
-
-    notesApp->init();
-
-    return app.exec();
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
+  qmlRegisterType<Scanner>("AudioNotes", 1, 0, "Scanner");
+  qmlRegisterType<AudioNotesApp>("AudioNotes", 1, 0, "AudioNotesApp");
+  qmlRegisterType<AudioNotesRepo>("AudioNotes", 1, 0, "AudioNotesRepo");
+  qmlRegisterType<AudioNotesReposModel>("AudioNotes", 1, 0, "AudioNotesReposModel");
+  qmlRegisterType<AudioNote>("AudioNotes", 1, 0, "AudioNote");
+  qmlRegisterType<AudioNotesModel>("AudioNotes", 1, 0, "AudioNotesModel");
+  qmlRegisterType<AudioNotePlayback>("AudioNotes", 1, 0, "AudioNotePlayback");
+  qmlRegisterType<AudioNoteCreator>("AudioNotes", 1, 0, "AudioNoteCreator");
+  auto* notesApp = new AudioNotesApp();
+  engine.rootContext()->setContextProperty("notesApp", notesApp);
+  const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
+                   [url](const QObject * obj, const QUrl & objUrl)
+  {
+    if ((obj == nullptr) && url == objUrl)
+      QCoreApplication::exit(-1);
+  },
+  Qt::QueuedConnection);
+  engine.load(url);
+  notesApp->init();
+  return QGuiApplication::exec();
 }
